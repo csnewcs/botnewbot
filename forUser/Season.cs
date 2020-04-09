723ecaf898192c34fd75c358bbfc7aae550ba3ee
csnewcs <hj666c3@gmail.com>
2020-04-09 20:59:46 +0900

    버그픽스

---
+++ forUser/Season.cs
@@ -0,0 +1,63 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Collections;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Newtonsoft.Json.Linq;
+
+namespace bot
+{
+    class Season : ModuleBase<SocketCommandContext>
+    {
+        public void mkdt(DiscordSocketClient client)
+        {
+            int last = 0;
+            while (true)
+            {
+                DateTime time = DateTime.Now;
+                int now = time.Second;
+                if ((now == 3 || now == 6 || now == 9 || now == 12) && now != last)
+                {
+                    DirectoryInfo servers = new DirectoryInfo("servers");
+                    foreach (DirectoryInfo server in servers.GetDirectories()) //
+                    {
+                        var users = server.GetFiles();
+                        SortedList list = new SortedList();
+                        foreach (var user in users)
+                        {
+                            if (user.Name == "config.json" || user.Name == "season.json") continue;
+                            string result = user.OpenText().ReadToEnd();
+                            JObject jObject = JObject.Parse(result);
+                            ulong money = (ulong)jObject["money"];
+                            list.Add(money, ulong.Parse(user.Name));
+                        }
+                        
+                        JArray save = new JArray();
+                        try
+                        {
+                            save = JArray.Parse(File.ReadAllText($"servers/{server.Name}/season.json"));
+                        }
+                        finally
+                        {
+                            string date = $"{time.Year}.{time.Month}.{time.Day}";
+                            SocketGuild guild = client.GetGuild(ulong.Parse(server.Name));
+                            SocketGuildUser guildUser = guild.GetUser((ulong)list.GetByIndex(1));
+                            JObject single = new JObject();
+                            single.Add("date", date);
+                            single.Add("username", Program.getNickname(guildUser));
+                            single.Add("money", (ulong)list.GetKey(1));
+                            save.Add(single);
+                            File.WriteAllText($"servers/{server.Name}/season.json", save.ToString());
+                        }
+                        last = now;
+                        Console.WriteLine("{0}자 시즌 저장 완료", time.Second);
+                    }
+                }
+                Thread.Sleep(360); //1시간
+            }
+        }
+    }
+}
